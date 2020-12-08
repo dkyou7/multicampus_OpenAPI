ae3e7ea4cacb5dc06dc7d71ef47bd2fc846cefb4
youdongkwan <dkyou7@naver.com>
2020-12-08 14:19:08 +0900

    JPA 사용해서 CRUD 작성해보기

--- restPractice/src/main/java/com/example/contact/ContactController_Rest.java
+++ restPractice/src/main/java/com/example/contact/ContactController_Rest.java
@@ -1,9 +1,6 @@
 package com.example.contact;
 
-import com.example.error.ApiException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
